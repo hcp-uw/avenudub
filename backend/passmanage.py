d6df59c94deb641df102109825ed312d6daeb94a
Joonho Choi <joonhojams@gmail.com>
2025-01-21 19:22:30 -0800

    revised the venv setup txt file after working thru it w/ nimai

--- backend/passmanage.py
+++ backend/passmanage.py
@@ -1,20 +1,18 @@
 import bcrypt
+# TODO: conversion for use with MySQL database
+
 
 def encrypt(password):
-    with open("config", "w") as f:
-        bytes = password.encode("utf-8")
-        salt = bcrypt.gensalt()
-        f.write(str(bcrypt.hashpw(bytes,salt).decode()))
+    bytes = password.encode("utf-8")
+    salt = bcrypt.gensalt()
     
 
 def passcheck(password): #checks to see if root password is correct
-    with open("config", "r") as f:
-        rootpass = bytes(f.readlines()[0], "utf-8")
-        userBytes = password.encode("utf-8")
-        return bcrypt.checkpw(userBytes, rootpass)
+    rootpass = bytes(f.readlines()[0], "utf-8")
+    userBytes = password.encode("utf-8")
+    return bcrypt.checkpw(userBytes, rootpass)
     
 def syslogin():
-    with open("config", "r") as f:
-        rootpass = bytes(f.readlines()[0], "utf-8")
-        return rootpass.decode("utf-8")
-        # return bcrypt.checkpw(userBytes, rootpass)
+
+    rootpass = bytes(f.readlines()[0], "utf-8")
+    return rootpass.decode("utf-8")
