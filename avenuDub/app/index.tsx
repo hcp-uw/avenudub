d792b9d2e8566f843a86f78ffea8b1e1cb7fb5db
SmaPeriodFour <samiautrey@gmail.com>
2025-01-21 19:00:53 -0800

    Update index.tsx

--- avenuDub/app/index.tsx
+++ avenuDub/app/index.tsx
@@ -1,24 +1,30 @@
 import { Text, View } from "react-native";
 import {ParamListBase, useNavigation} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Button } from '@react-navigation/elements';
 
 
 export default function Index() {
   const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>()
-
+  const Tab = createBottomTabNavigator();
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        position: 'sticky',
+        top: 0,
+        zIndex: 1,
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Button onPress={() => navigation.navigate('pages/business')}>
-        Business
+      <Button color = '#5e30b3' onPress={() => navigation.navigate('pages/business')}>
+        Businesses
+      </Button>
+      <Button color = '#5e30b3' onPress={() => navigation.navigate('pages/safety')}>
+        Safety Alerts
       </Button>
     </View>
   );
