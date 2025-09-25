9c35e5a34505a694a3d0b6003b98beffce2d4858
nimaib2 <nimaib@uw.edu>
2025-09-25 16:27:18 -0700

    uncommented getting crime log from website for testing

--- backend/beautifulSoup.py
+++ backend/beautifulSoup.py
@@ -6,40 +6,39 @@ from PIL import Image, ImageOps, ImageEnhance
 import pytesseract
 import cv2
 import numpy as np
-import re
 
 def scrapePDF():
-    # url = "https://police.uw.edu/60-day-log/attachment/04162025/"
-    # headers = {
-    #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
-    # }
+    url = "https://police.uw.edu/60-day-log/attachment/04162025/"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
+    }
 
-    # response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers)
 
-    # if response.status_code == 200:
-    #     soup = BeautifulSoup(response.text, 'html.parser')
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-    #     pdf_links = soup.find_all('a', href=True)  # Find all anchor tags with hrefs
-    #     pdf_link = ""
-    #     for link in pdf_links:
-    #         href = link.get('href')
-    #         if "https://police.uw.edu/wp-content/uploads/" in href and href.endswith(".pdf"):
-    #             pdf_link = href
-    #             print(f"Found PDF link: {pdf_link}")
-    #             break
+        pdf_links = soup.find_all('a', href=True)  # Find all anchor tags with hrefs
+        pdf_link = ""
+        for link in pdf_links:
+            href = link.get('href')
+            if "https://police.uw.edu/wp-content/uploads/" in href and href.endswith(".pdf"):
+                pdf_link = href
+                print(f"Found PDF link: {pdf_link}")
+                break
 
-    #     if pdf_link:
-    #         pdf_response = requests.get(pdf_link, headers=headers)
-    #         if pdf_response.status_code == 200:
-    #             with open("latest_log.pdf", "wb") as file:
-    #                 file.write(pdf_response.content)
-    #             print("PDF downloaded successfully!")
-    #         else:
-    #             print(f"Failed to download the PDF. HTTP Status Code: {pdf_response.status_code}")
-    #     else:
-    #         print("No PDF link found on the page.")
-    # else:
-    #     print(f"Failed to load the main page. HTTP Status Code: {response.status_code}")
+        if pdf_link:
+            pdf_response = requests.get(pdf_link, headers=headers)
+            if pdf_response.status_code == 200:
+                with open("latest_log.pdf", "wb") as file:
+                    file.write(pdf_response.content)
+                print("PDF downloaded successfully!")
+            else:
+                print(f"Failed to download the PDF. HTTP Status Code: {pdf_response.status_code}")
+        else:
+            print("No PDF link found on the page.")
+    else:
+        print(f"Failed to load the main page. HTTP Status Code: {response.status_code}")
 
     raw_data = []
     page_num = 1
