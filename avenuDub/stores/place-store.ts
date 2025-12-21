2c15d34837ef51305bbb2afe951fa7109c703080
megan-yam <meganyammm@gmail.com>
2025-12-20 22:53:29 -0800

    modified account store + started working on place store

---
+++ avenuDub/stores/place-store.ts
@@ -0,0 +1,34 @@
+import React from "react";
+import { makeObservable, action, observable, makeAutoObservable } from 'mobx';
+import { Place } from "@/types/place";
+
+// see types/place.ts
+class PlaceStore {
+    places: Place[] = [];
+    selectedPlace?: Place;
+    loading = false;
+
+    constructor() {
+        makeAutoObservable(this);
+    }
+
+    get placeCount() {
+        return this.places.length
+    }
+
+    get hasSelectedPlace() {
+        return this.selectedPlace !== undefined
+    }
+
+    setPlaces(places: Place[]) {
+        this.places = places
+    }
+
+    selectPlace(place: Place) {
+        this.selectedPlace = place
+    }
+
+    clearSelection() {
+        this.selectedPlace = undefined
+    }
+}
